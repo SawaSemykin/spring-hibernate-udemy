51520821efc09aac91140d57068a33d927e8a3d0
Alexander Semykin <sawasemykin@yandex.ru>
2018-09-06 07:00:05 +0300

    Initial commit for spring-demo-aop-zz-around

---
+++ spring-demo-aop-zz-around/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,38 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(
+			ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+		
+		// print out method we are advising on
+		String method = theProceedingJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @Around on method: " + method);
+		
+		// get begin timestamp
+		long begin = System.currentTimeMillis();
+		
+		// now, let's execute the method
+		Object result = theProceedingJoinPoint.proceed();
+		
+		// get end timestamp
+		long end = System.currentTimeMillis();
+		
+		// compute duration and display it
+		long duration = end - begin;
+		System.out.println("\n=====> Duration: " + duration / 1000 + " seconds");
+		
+		return result;
+	}
+
+}
